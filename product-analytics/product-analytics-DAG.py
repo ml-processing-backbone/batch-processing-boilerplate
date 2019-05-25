19e8e1663a4152245e54d4b3988b52226187374e
Tansu Dasli <tansudasli@gmail.com>
2019-05-25 04:18:48 +0300

    average-product-prices (tasks updated w/ valid Operators and Sensors)

--- product-analytics/product-analytics-DAG.py
+++ product-analytics/product-analytics-DAG.py
@@ -3,6 +3,8 @@ import logging
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.sensors.sftp_sensor import SFTPSensor
+from airflow.contrib.sensors.gcs_sensor import GoogleCloudStorageObjectSensor
+from airflow.contrib.operators.sftp_operator import SFTPOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.dataflow_operator import DataFlowPythonOperator
 from airflow.contrib.operators.file_to_gcs import FileToGoogleCloudStorageOperator
@@ -37,24 +39,34 @@ with DAG('product-analytics', default_args=default_args, schedule_interval=timed
 
     # TODO: not implemented for now
     # copy from sftp to gcp storage
-    t2 = BashOperator(
-        task_id='copy-for-datalake',
+    t2 = SFTPOperator(
+        task_id='transfer-to-datalake',
         bash_command=''
     )
 
     # copy from gcs to process bucket for analytical calculations
-    t3 = BashOperator(
+    t3 = GoogleCloudStorageToGoogleCloudStorageOperator(
         task_id='copy-for-analytics',
         bash_command=''
     )
 
-    # git clone average-prices-by-product-enhanced.py file
-    # deploy to GCP dataflow as a beam job
-
-    # check GCP dataflow job status
+    # git clone average-prices-by-product-enhanced.py file ?????
+    # deploy to GCP dataflow as a beam job, and check GCP dataflow job status
+    t4 = DataFlowPythonOperator(
+        task_id='copy-for-analytics',
+        bash_command=''
+    )
 
     # Listen output folder w/ sensor
+    t5 = GoogleCloudStorageObjectSensor(
+        task_id='copy-for-analytics',
+        bash_command=''
+    )
     # transfer output file to SFTP server
-
+    t6 = SFTPOperator(
+        task_id='transfer-to-datalake',
+        bash_command=''
+    )
 
     t3 >> t2 >> t1
+
