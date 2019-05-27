7332715807720b7e8238ba90a3163de0bee36668
Tansu Dasli <tansudasli@gmail.com>
2019-05-27 12:56:15 +0300

    sftp simulation completed

--- product-analytics/prepare-environment.sh
+++ product-analytics/prepare-environment.sh
@@ -1,10 +1,44 @@
 #!/usr/bin/env bash
 
-export BUCKET_NAME="sandbox-datalake-123455"
+echo "edit env variables "
+echo "install airflow-cluster and enable dataflow apis etc.."
+echo "run scripts inside product-analytics folder !!"
 
-# create buckets
-gsutil mb -b on gs://${BUCKET_NAME}
+export GCP_PROJECT_NAME=sandbox-236618
+
+export BEAM_BUCKET_NAME=beam-pipelines-123
+export BEAM_FILE_NAME=average-prices-by-product-enhanced
+
+export INGESTION_BUCKET_NAME=sandbox-datalake-123
+
+export AIRFLOW_BUCKET_NAME=europe-west1-airflow-cluste-0bf08843-bucket
+
+
+echo "***** create buckets for ingestion"
+gsutil mb -b on gs://${INGESTION_BUCKET_NAME}
 
 # ${BUCKET_NAME}/incoming
 # ${BUCKET_NAME}/datalake
 # ${BUCKET_NAME}/processing
+
+echo "***** transfer DAG file into airflow"
+gsutil cp product-analytics/product-analytics-DAG.py gs://${AIRFLOW_BUCKET_NAME}/dags
+
+
+
+# -------------------------------  simulation -------------------------
+echo "simulate ingestion of input files instead of SFTP thing"
+
+
+echo "***** transfer files for ingestion"
+
+for FILE_SUFFIX in 001 002 003 004 005
+do
+   gsutil cp ./dataset/sales_transactions_${FILE_SUFFIX}.csv gs://${INGESTION_BUCKET_NAME}
+
+   sleep 1m
+
+done
+
+
+
